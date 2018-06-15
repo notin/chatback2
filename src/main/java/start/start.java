7eb9d1da96a2fa075d9fa74c426995d9de94b1dd
chadforgit <chadforgit@gmail.com>
2018-06-15 15:27:57 -0700

    spring

--- src/main/java/start/start.java
+++ src/main/java/start/start.java
@@ -3,12 +3,17 @@ package start;
 import controllers.Messager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import pojos.converation.Conversation;
+
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class start
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(start.class, args);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(start.class, args);
+        Logger.getAnonymousLogger().info(String.valueOf(configurableApplicationContext.getStartupDate()));
     }
 }
