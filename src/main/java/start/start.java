1ae361ed6188443dbb91d51893a5f9ce4dabad2a
chadforgit <chadforgit@gmail.com>
2018-06-15 15:20:31 -0700

    spring

--- src/main/java/start/start.java
+++ src/main/java/start/start.java
@@ -1,6 +1,7 @@
 package start;
 
 import controllers.Messager;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pojos.converation.Conversation;
 @SpringBootApplication
@@ -8,7 +9,6 @@ public class start
 {
     public static void main(String[] args)
     {
-        Messager messager = new Messager();
-        messager.receive(new Conversation());
+        SpringApplication.run(start.class, args);
     }
 }
