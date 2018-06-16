a803697b0579443c113f2e1271ba0a1d629524fc
chadforgit <chadforgit@gmail.com>
2018-06-15 17:02:09 -0700

    spring

--- src/main/java/com/chatback21/Application.java
+++ src/main/java/com/chatback21/Application.java
@@ -2,12 +2,18 @@ package com.chatback21;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
-public class Application {
-
+@ComponentScan(basePackages = "com.chatback21")
+public class Application
+{
 	public static void main(String[] args)
 	{
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(Application.class, args);
+		configurableApplicationContext.getEnvironment().getSystemProperties().keySet().stream().forEachOrdered(x->Logger.getAnonymousLogger().info(x));
 	}
 }
--- src/test/java/com/chatback21/ApplicationTests.java
+++ src/test/java/com/chatback21/ApplicationTests.java
@@ -1,8 +1,10 @@
 package com.chatback21;
 
+import com.chatback21.utils.Caller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -10,7 +12,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads()
+	{
+		Caller caller = new Caller();
+		Object object =caller.makeCall("localhost:8080", null, null, HttpMethod.GET, Object.class, null);
+		object.equals("");
 	}
 
 }
