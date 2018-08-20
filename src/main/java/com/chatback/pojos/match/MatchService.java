e7cc4f065d7a696a1d4048622a09b28a47ca0d61
chadforgit <chadforgit@gmail.com>
2018-08-20 08:25:16 -0700

    got matching done

--- src/main/java/com/chatback/pojos/match/MatchService.java
+++ src/main/java/com/chatback/pojos/match/MatchService.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.*;
-import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
 
@@ -19,80 +18,128 @@ public class MatchService
     private MatchRepository matchRepository;
 
 
-   private  static List<Match> members = new ArrayList<>();
-    private static Executor executor;
-    private static String guis=null;
+   private static List<Match> members = new ArrayList<>();
+    private static String guis = null;
 
 
     public static Match getMatch(Match match)
     {
-        Match matchCompletableFuture = getMatchCompletableFuture(match, MatchService::getMember);
+        Match matchCompletableFuture = getMatchCompletableFuture(match);
         Logger.getAnonymousLogger().info("has matched");
         return matchCompletableFuture;
     }
 
-    private static Match getMatchCompletableFuture(Match match, Function<Match,Match> function) {
+    private static Match getMatchCompletableFuture(Match match)
+    {
         members.add(match);
         Match match1 = null;
-        executor = Executors.newFixedThreadPool(10);
-        CompletableFuture<Match> completableFuture = new CompletableFuture<>().supplyAsync(new Supplier<Match>()
-                                                                                                                    {
-                                                                                                                        @Override
-                                                                                                                        public Match get()
-                                                                                                                        {
-                                                                                                                            Match member =null;
-                                                                                                                            while(member == null)
-                                                                                                                            {
-                                                                                                                                try {
-                                                                                                                                    TimeUnit.SECONDS.sleep(3);
+        Executor executor = Executors.newFixedThreadPool(5);
+        match1 = getMatch(match, executor);
+        removeMatch(match, executor);
+        return match1;
+    }
 
-                                                                                                                                    Logger.getAnonymousLogger().info("attempting match");
-                                                                                                                                    member = getMember(match);
-                                                                                                                                } catch (Exception e) {
-                                                                                                                                    throw new IllegalStateException(e);
-                                                                                                                                }
-                                                                                                                            }
-                                                                                                                            return member;
-                                                                                                                        }
-                                                                                                                    }, executor);
+    private static Match getMatch(Match match, Executor executor)
+    {
+        Match match1 = null;
+        CompletableFuture<Match> completableFuture = getAttempting_match(match, executor);
         try
         {
-             match1 = completableFuture.get();
+            match1 = completableFuture.get();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        removePairFromList(match, match1);
         return match1;
+    }
+
+    private static void removeMatch(Match match, Executor executor)
+    {
+        CompletableFuture<Match> completableFuture = getAttemptingRemoval(match, executor);
+        try
+        {
+            completableFuture.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static CompletableFuture<Match> getAttempting_match(Match match, Executor executor)
+    {
+
+        return new CompletableFuture<>().supplyAsync(new Supplier<Match>() {
+                                                        @Override
+                                                        public Match get()
+                                                        {
+                                                            Match member =null;
+                                                            while(member == null)
+                                                            {
+                                                                try
+                                                                {
+                                                                    TimeUnit.SECONDS.sleep(2);
+                                                                    Logger.getAnonymousLogger().info("attempting match");
+                                                                    member = getMember(match);
+                                                                }
+                                                                catch (Exception e)
+                                                                {
+                                                                    throw new IllegalStateException(e);
+                                                                }
+                                                            }
+                                                            return member; }}, executor);
+    }
+
+    private static CompletableFuture<Match> getAttemptingRemoval(Match match, Executor executor)
+    {
+        return new CompletableFuture<>().supplyAsync(new Supplier<Match>() {
+            @Override
+            public Match get()
+            {
+                Match member =null;
+                while(member == null)
+                {
+                    try
+                    {
+                        TimeUnit.SECONDS.sleep(2);
+                        Logger.getAnonymousLogger().info("attempting remaoval after successful match");
+                        removePairFromList(match);
+                    }
+                    catch (Exception e)
+                    {
+                        throw new IllegalStateException(e);
+                    }
+                }
+                return member; }}, executor);
     }
 
     private static Match getMember(Match match) {
 
-        Match match1 = null;
-        while(match1 == null)
+        Match partner = null;
+        while(partner == null && match.getPartner() ==null)
         {
-            match1 = members.stream().filter(x -> !x.getSelf().equalsIgnoreCase(match.getSelf())).findFirst().orElse(null);
-
+            partner = getMembers().stream().filter(x -> !x.getSelf().equalsIgnoreCase(match.getSelf())).findFirst().orElse(null);
         }
         if(guis == null)
         {
             guis = getGUIS();
         }
-        settingValues(match, match1, guis);
+        settingValues(match, partner, guis);
         guis = null;
         return match;
     }
 
-    private static void removePairFromList(Match match, Match match1) {
-        members.remove(match);
-        members.remove(match1);
+    private static void removePairFromList(Match match)
+    {
+        getMembers().remove(match);
     }
 
-    private static void settingValues(Match match, Match match1, String guis) {
+    private static void settingValues(Match match, Match match1, String guis)
+    {
         match.setPartner(match1.getSelf());
         match1.setPartner(match.getSelf());
+
         List<String> guisList = new ArrayList<>();
         guisList.add(match.getConversation());
         guisList.add(match1.getConversation());
+
         String guiPlace = guisList.stream().filter(x->x!=null).findFirst().orElse(null);
         if(guiPlace == null)
         {
@@ -115,4 +162,8 @@ public class MatchService
             return randomUUIDString;
     }
 
+    private synchronized static List<Match> getMembers()
+    {
+        return members;
+    }
 }
