40601f2f71cf4e099034cecbff5b407528d87db2
chadforgit <chadforgit@gmail.com>
2019-05-24 14:49:20 -0700

    first

--- src/main/resources/reaction.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE conversation(
-   type  VARCHAR(8) NOT NULL PRIMARY KEY
-  ,gui_1 VARCHAR(4) NOT NULL
-  ,gui_2 VARCHAR(3) NOT NULL
-);
-INSERT INTO conversation(type,gui_1,gui_2) VALUES ('starting','good','bad');
-INSERT INTO conversation(type,gui_1,gui_2) VALUES ('starting','good','bad');
